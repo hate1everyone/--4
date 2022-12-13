0f9ff1461bb8034063f5252281a4c8d647dacb4f
hate1everyone <118364577+hate1everyone@users.noreply.github.com>
2022-12-13 17:11:16 +0300

    Add files via upload

--- 123.pas
+++ 123.pas
@@ -1,5 +1,5 @@
 ﻿uses graphABC;
-var x0, y0, mx, my, x1, y1, n: integer;
+var x0, y0, mx, my, x1, y1, n, x2: integer;
     l, p, x, y, a, b, h: real;   
 begin
   x0:= 250;
@@ -23,13 +23,16 @@ begin
     x += 0.0001
   end; 
   read(a, b, n);
-  h:= b - a/n;
+  writeln('Реализовать программу площади фигуры, с применением метода левых прямоугольников');
+  h:= (b - a)/n;
   x:= a;
   while x <= b do begin 
     y:= 1 * x ** 3 + (2) * x ** 2 +(-3) * x + (15);
     x1:= x0 + round(x*mx);
     y1:= y0 - round(y*my);
-    line(x1, y1, x1, y0);
+    x2:= x0 + round ((h+x)*mx);
+    setpencolor(clrandom);
+    rectangle(x1, y1, x2, y0);
     x += h;
   end;
 end.
